4924a1e977d99da57020723a5564e37990e2dffe
harryhancock <61349549+harryhancock@users.noreply.github.com>
2021-03-16 22:06:50 +0000

    Add files via upload

---
+++ Lidar_code/lidar_pi.py
@@ -0,0 +1,67 @@
+   
+# -*- coding: utf-8 -*
+#see teachmemirco uart serial raspberry pi tutorial webpage for serial.Serial bit_length
+#see tfmini github for the rest of the file and comments
+
+import RPi.GPIO as GPIO
+import serial
+import time
+
+ser = serial.Serial(            
+     port='/dev/serial0',
+     baudrate = 115200,
+     parity=serial.PARITY_NONE,
+     stopbits=serial.STOPBITS_ONE,
+     bytesize=serial.EIGHTBITS,
+     timeout=1
+ )
+
+def getTFminiData():
+    while True:
+        #time.sleep(0.1)
+        count = ser.in_waiting
+        if count > 8:
+            recv = ser.read(9)   
+            ser.reset_input_buffer() 
+            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
+            # type(recv[0]), 'str' in python2, 'int' in python3 
+            
+            if recv[0] == 0x59 and recv[1] == 0x59:     #python3
+                distance = recv[2] + recv[3]
+                * 256
+                strength = recv[4] + recv[5] * 256
+                print('(', distance, ',', strength, ')')
+                ser.reset_input_buffer()
+                
+            if recv[0] == 'Y' and recv[1] == 'Y':     #python2
+                lowD = int(recv[2].encode('hex'), 16)      
+                highD = int(recv[3].encode('hex'), 16)
+                lowS = int(recv[4].encode('hex'), 16)      
+                highS = int(recv[5].encode('hex'), 16)
+                distance = lowD + highD * 256
+                strength = lowS + highS * 256
+                print(distance, strength)
+            
+            # you can also distinguish python2 and python3: 
+            #import sys
+            #sys.version[0] == '2'    #True, python2
+            #sys.version[0] == '3'    #True, python3
+
+
+if __name__ == '__main__':
+    try:
+        if ser.is_open == False:
+            ser.open()
+        getTFminiData()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
+    finally:
+        GPIO.cleanup()
+
+'''      
+try:
+  while True:
+    print("hello")
+    #time.delay(10)
+'''
